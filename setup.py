3910b5fd90f2a66fc3588d29a069c92e795437f7
nel215 <otomo.yuhei@gmail.com>
2016-07-01 00:47:56 +0900

    added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mondrianforest',
+    version='0.0.1.0',
+    author='nel215',
+    author_email='otomo.yuhei@gmail.com',
+    url='https://github.com/nel215/mondrianforest',
+    py_modules=['mondrianforest'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+    ],
+    keywords=['machine learning'],
+)
