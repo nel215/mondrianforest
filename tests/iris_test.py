057ba525dba851827b633f9f2b977e51afe363a6
nel215 <otomo.yuhei@gmail.com>
2016-06-30 22:49:19 +0900

    fixed test

--- tests/iris_test.py
+++ tests/iris_test.py
@@ -8,6 +8,15 @@ def test_iris():
     np.random.seed(215)
     iris = datasets.load_iris()
     tree = mondrianforest.MondrianTreeClassifier()
-    cv = cross_validation.ShuffleSplit(len(iris.data), n_iter=10, test_size=0.05)
+    cv = cross_validation.ShuffleSplit(len(iris.data), n_iter=20, test_size=0.10)
     scores = cross_validation.cross_val_score(tree, iris.data, iris.target, cv=cv)
+    assert scores.mean() > 0.85
+
+
+def test_forest_iris():
+    np.random.seed(215)
+    iris = datasets.load_iris()
+    forest = mondrianforest.MondrianForestClassifier(n_tree=10)
+    cv = cross_validation.ShuffleSplit(len(iris.data), n_iter=20, test_size=0.10)
+    scores = cross_validation.cross_val_score(forest, iris.data, iris.target, cv=cv)
     assert scores.mean() > 0.9
