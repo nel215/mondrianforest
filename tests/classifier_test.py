b1576c05e680c519b12f2e17ddc5b70cccfa6e55
nel215 <otomo.yuhei@gmail.com>
2016-07-02 20:52:58 +0900

    fixed typo

--- tests/classifier_test.py
+++ tests/classifier_test.py
@@ -12,9 +12,9 @@ def test_classifier_stat_update():
     assert stat.stats[0]['sq_sum'].tolist() == [2, 8, 25]
     assert stat.stats[0]['count'] == 2
 
-    prop = stat.predict_proba(np.array([0, -1, -2]))
+    prob = stat.predict_proba(np.array([0, -1, -2]))
     sum_prob = 0.0
-    for k, v in prop.items():
+    for k, v in prob.items():
         sum_prob += v
     assert abs(v - 1.0) < 1e-9
-    assert prop[1] > prop[0]
+    assert prob[1] > prob[0]
