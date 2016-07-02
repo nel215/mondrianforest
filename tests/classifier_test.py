b3502d2cfcb0e47334e9a86566b8a51c8649cc41
nel215 <otomo.yuhei@gmail.com>
2016-07-02 18:24:51 +0900

    separated test of gaussian classifier

---
+++ tests/classifier_test.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+import mondrianforest
+import numpy as np
+
+
+def test_classifier_stat_update():
+    stat = mondrianforest.Classifier()
+    stat.add(np.array([1, 2, 3]), 0)
+    stat.add(np.array([1, 2, 4]), 0)
+    stat.add(np.array([0, -1, -2]), 1)
+    assert stat.stats[0]['sum'].tolist() == [2, 4, 7]
+    assert stat.stats[0]['sq_sum'].tolist() == [2, 8, 25]
+    assert stat.stats[0]['count'] == 2
+
+    prop = stat.predict_proba(np.array([0, -1, -2]))
+    assert prop[1] > prop[0]
--- tests/node_test.py
+++ tests/node_test.py
@@ -19,16 +19,3 @@ def test_partial_fit():
 
     proba = tree.predict_proba(np.array([[1.0, 2.0]]))[0]
     assert proba[1] > proba[0]
-
-
-def test_classifier_stat_update():
-    stat = mondrianforest.Classifier()
-    stat.add(np.array([1, 2, 3]), 0)
-    stat.add(np.array([1, 2, 4]), 0)
-    stat.add(np.array([0, -1, -2]), 1)
-    assert stat.stats[0]['sum'].tolist() == [2, 4, 7]
-    assert stat.stats[0]['sq_sum'].tolist() == [2, 8, 25]
-    assert stat.stats[0]['count'] == 2
-
-    prop = stat.predict_proba(np.array([0, -1, -2]))
-    assert prop[1] > prop[0]
